9e6cf7ed25f0992bd46ca60b2b504af4c4ce78bb
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-18 17:57:34 +0530

    Add eslint configurations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Express } from "express";
+
 import serverConfig from "./config/serverConfig";
 
+
+
 const app: Express = express();
+const x = 1;
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started to listen at :${serverConfig.PORT}`);
   console.log("wow");
-  
 });
