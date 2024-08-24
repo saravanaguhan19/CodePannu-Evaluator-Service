b2db9b5e7d126b387b80de2a38fd1954aef94dbe
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-24 13:39:17 +0530

    Fix: change validator file name

--- src/routes/v1/submissionRoutes.ts
+++ src/routes/v1/submissionRoutes.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 import { addSubmission } from "../../controllers/submissionController";
 import { createSubmissionZodSchema } from "../../dtos/CreateSubmissionDto";
-import { validate } from "../../validators/createSubmissionValidators";
+import { validate } from "../../validators/zodValidator";
 
 const submissionRouter = express.Router();
 
