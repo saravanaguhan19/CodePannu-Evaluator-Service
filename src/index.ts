2966c14ae1e97930b849539e32fad54368197d95
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-08-24 11:27:29 +0530

    Feat:Add bullmq board ui

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -9,13 +10,31 @@ const app: Express = express();
 
 app.use("/api", apiRouter);
 
+app.use("/ui", bullBoardAdapter.getRouter());
+
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started to listen at :${serverConfig.PORT}`);
 
+  console.log("bull board running");
+
   SampleWorker("SampleQueue");
-  sampleQueueProducer("SampleJob", {
-    name: "saravana",
-    company: "Vetti",
-    location: "chennai",
-  });
+  sampleQueueProducer(
+    "SampleJob",
+    {
+      name: "saravana ",
+      company: "Vetti",
+      location: "chennai",
+    },
+    5
+  );
+
+  sampleQueueProducer(
+    "SampleJob",
+    {
+      name: "saravana 2 priority",
+      company: "Vetti ccc",
+      location: "chennai cc",
+    },
+    2
+  );
 });
